2c755dd18c48f754d9263a44fed84fc761849e7b
Isaque F <isaquefrankli@gmail.com>
2021-07-09 11:14:56 -0300

    contact page done

--- src/components/Contato.js
+++ src/components/Contato.js
@@ -1,9 +1,21 @@
 import React from 'react'
+import { Container, Row, Col } from 'react-bootstrap'
+
 
 function Contato() {
     return (
-        <div>
-            <h1>Otário</h1>
+        <div className="contact">
+            <br></br>
+            <br></br>
+            <h3 className="titulo2">Entre em contato</h3>
+            <hr></hr>
+            <img src="img/contato.png" alt="imagem de contato" className="homeimgs" />
+            <p className="paragraph2">Entre em contato conosco agora mesmo por um dos meios abaixo ou das nossa redes sociais.</p>
+            <br></br>
+            <a href=""><button>Enviar Whatsapp</button></a>
+            <a href="mailto:"><button>Enviar email</button></a> 
+            <br></br>
+            <br></br>
         </div>
     )
 }
