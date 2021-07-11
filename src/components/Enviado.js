9b34aa256ea455231f381c479c3ce5ec739ceae8
Isaque F <isaquefrankli@gmail.com>
2021-07-11 15:33:23 -0300

    panel almost finished

--- src/components/Enviado.js
+++ src/components/Enviado.js
@@ -5,7 +5,8 @@ function Enviado() {
         <div className="contact">
             <br></br>
             <br></br>
-            <h3 className="titulo2">Seu pedido de orçamento foi enviado</h3>
+            <br></br>
+            <h3 className="titulo2">Seu pedido de orçamento foi enviado.</h3>
             <hr></hr>
             <img src="img/message.png" alt="imagem de envio" className="homeimgs" />
             <p className="paragraph2">Entraremos em contato em breve com o retorno.</p>
--- src/components/Painel.js
+++ src/components/Painel.js
@@ -1,10 +1,63 @@
-import React from 'react'
+import React, { useContext, useEffect, useState } from 'react'
+import FirebaseContext from '../firebase/context'
+import { Container, Card, Row, Col } from 'react-bootstrap'
+import { Helmet } from 'react-helmet'
 
-function Painel() {
+function Painel(props) {
+
+    const { firebase } = useContext(FirebaseContext)
+    const [posts, setPosts] = useState([])
+    const [loading, setLoading] = useState(false)
+    const isTopPage = props.location.pathname.includes('painel');
+
+    useEffect(() => {
+        getBudgets()
+    }, [isTopPage])
+
+    function getBudgets() {
+        return firebase.db
+        .collection('orçamentos')
+        .orderBy('created', 'desc')
+        .onSnapshot(handleSnapshot)
+    }
+
+    function handleSnapshot(snapshot) {
+        const links = snapshot.docs.map(doc => {
+            return { id: doc.id, ...doc.data() }
+        })
+        setPosts(links)
+        setLoading(false)
+    }
+
     return (
-        <div>
-            <h1>Fala aí palhaço, parte 2.</h1>
-        </div>
+        <>
+        <Helmet>
+            <meta charSet="utf-8" />
+            <title>Painel</title>
+        </Helmet>
+        <Container>
+            <div style={{ opacity: loading ? 0.25 : 1}} className="fundo2">
+                <br></br>
+                <a href="create"><button>Criar novo post</button></a>
+                <br></br>
+                <br></br>
+                <br></br>
+                <h3 className="titulo2">Orçamentos requisitados aguardando retorno:</h3>
+                {posts.map((post) => (
+                    <Card style={{ marginTop: '50px', marginBottom: '20px', borderRadius: '20px'}}>
+                        <Card.Body>
+                        <small>Nome:</small><h6 className="paragraph2">{post.nome}</h6>
+                        <small>Telefone:</small><p className="paragraph2">{post.telefone}</p>
+                        <small>Email:</small><p className="paragraph2">{post.email}</p>
+                        <small>Descrição:</small><div dangerouslySetInnerHTML={{ __html: post.description }} />
+                        </Card.Body>
+                    </Card>
+                ))}
+                <br></br>
+                <br></br>
+            </div>
+        </Container>
+        </>
     )
 }
 
